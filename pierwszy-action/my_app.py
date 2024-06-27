18eac02fee2cfda879db76adda20fccc8dafa8d8
apecmat <surowiec.mat@gmail.com>
2024-06-27 21:54:02 +0200

    My first action is ready

---
+++ pierwszy-action/my_app.py
@@ -0,0 +1,40 @@
+import os
+import yaml
+
+parameters = {
+    'inputs': {},
+    'outputs': {}
+}
+
+action_file = 'action.yml'
+with open(action_file, 'r') as file:
+    action_data = yaml.safe_load(file)
+if 'inputs' in action_data:
+    for input_name, input_details in action_data['inputs'].items():
+        parameters['inputs'][input_name] = {
+            'description': input_details.get('description', ''),
+            'required': input_details.get('required', False),
+            'default': input_details.get('default', None)
+        }
+if 'outputs' in action_data:
+    for output_name, output_details in action_data['outputs'].items():
+        parameters['outputs'][output_name] = {
+            'description': output_details.get('description', '')
+        }
+
+print('Inputs:')
+for input_name, details in parameters['inputs'].items():
+    print(f'Input: {input_name}')
+    print(f'Description: {details["description"]}')
+    print(f'Required: {details["required"]}')
+    print(f'Default: {details["default"]}')
+    print()
+
+print('Outputs:')
+for output_name, details in parameters['outputs'].items():
+    print(f'Output: {output_name}')
+    print(f'Description: {details["description"]}')
+    print()
+
+with open(os.getenv('GITHUB_OUTPUT'), 'a') as f:
+    f.write(f'wynik=alejajki\n')
